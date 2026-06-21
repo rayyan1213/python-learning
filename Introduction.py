266d1fd227f1cffd5ed4c3476b038f001a6d7498
Rayyan Ali <rayyan9d@gmail.com>
2026-06-21 10:01:56 +0500

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,7 @@
+name = input("What's your name? ")
+age = input("your age?")
+language = input("your favorite language?")
+
+print( "Hello", name)
+print("your age is", age)
+print("your favortie language is", language)
