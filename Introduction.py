72dac3e551906f4b37121a1171a8ee807706b5ba
Rayyan Ali <rayyan9d@gmail.com>
2026-06-21 13:44:57 +0500

    Delete Introduction.py

--- Introduction.py
+++
@@ -1,7 +0,0 @@
-name = input("What's your name? ")
-age = input("your age?")
-language = input("your favorite language?")
-
-print( "Hello", name)
-print("your age is", age)
-print("your favortie language is", language)
